6abf35404c09a5ea1d6f80f11e7ac5604c9dcdc6
jlee <yeoniee217@gmail.com>
2020-03-27 18:03:02 -0500

    Created products controller.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ApplicationRecord
   belongs_to :category
+
+  validates :num, :name, :description, :price, presence: true
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :stock_quantity, numericality: { only_integer: true }
+
+  # has_one_attached :image
 end
