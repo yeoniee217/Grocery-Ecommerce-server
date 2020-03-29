516f2c8dccf6d5a18b1cbd5f9d2a3b0cf55fee5e
jlee <yeoniee217@gmail.com>
2020-03-28 21:43:20 -0500

    Added image attachment for Product model.

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,3 +1,12 @@
 ActiveAdmin.register Product do
-  permit_params :num, :name, :description, :price, :stock_quantity, :discount, :category_id
+  permit_params :num, :name, :description, :price, :stock_quantity, :discount, :category_id, :image
+
+  form do |f|
+    f.semantic_errors # shows errors on :base
+    f.inputs          # builds an input field for every attribute
+    f.inputs do
+      f.input :image, as: :file
+    end
+    f.actions         # adds the 'Submit' and 'Cancel' buttons
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -5,5 +5,5 @@ class Product < ApplicationRecord
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :stock_quantity, numericality: { only_integer: true }
 
-  # has_one_attached :image
+  has_one_attached :image
 end
