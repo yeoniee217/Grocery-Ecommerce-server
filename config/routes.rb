993b41777f50fc2e622dc81400e12b0a19d66aec
jlee <yeoniee217@gmail.com>
2020-03-28 19:51:52 -0500

    Added get products by category id api route.

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   scope "/categories" do
     get "/all_categories", to: "categories#all_categories"
   end
+
+  scope "/products" do
+    get "/by_category_id/:id", to: "products#by_category_id"
+  end
 end
