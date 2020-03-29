afc3fe17df1f1ba34f531c9a07095c082f5e14c4
jlee <yeoniee217@gmail.com>
2020-03-28 19:51:02 -0500

    Added permit params to active admin for categories and products table.

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,18 +1,3 @@
 ActiveAdmin.register Product do
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :num, :name, :description, :price, :stock_quantity, :discount, :category_id
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:num, :name, :description, :price, :stock_quantity, :discount, :category_id]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  permit_params :num, :name, :description, :price, :stock_quantity, :discount, :category_id
 end
