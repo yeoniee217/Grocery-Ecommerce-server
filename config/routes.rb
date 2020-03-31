728a22a028fa8be73f8e0eb459cdfc26f633df22
jlee72 <yeoniee217@gmail.com>
2020-03-30 21:32:59 -0500

    Added constraints for routes request format.

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  scope "/categories" do
-    get "/all_categories", to: "categories#all_categories"
-  end
+  constraints ->(request) { request.format == :json } do
+    scope "/categories" do
+      get "/all_categories", to: "categories#all_categories"
+    end
 
-  scope "/products" do
-    get "/by_category_id/:id", to: "products#by_category_id"
+    scope "/products" do
+      get "/by_category_id/:id", to: "products#by_category_id"
+    end
   end
 end
