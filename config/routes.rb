51c0e490af86f4daf34323445bd91e14abe02dc5
jlee72 <yeoniee217@gmail.com>
2020-04-12 02:44:16 -0500

    Added new routes to the new actions.

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,17 @@ Rails.application.routes.draw do
 
   constraints ->(request) { request.format == :json } do
     scope "/categories" do
-      get "/all_categories", to: "categories#all_categories"
+      get "", to: "categories#index"
+      get "/:id/products", to: "products#by_category_id"
+      get "/:id/products/onSale", to: "products#on_sale" # use query string?
+      get "/:id/products/new", to: "products#new"
+      get "/:id/products/recUpdated", to: "products#recently_updated"
     end
 
     scope "/products" do
-      get "/by_category_id/:id", to: "products#by_category_id"
+      get "/:id", to: "products#show"
     end
+
+    post "/search", to: "products#search"
   end
 end
